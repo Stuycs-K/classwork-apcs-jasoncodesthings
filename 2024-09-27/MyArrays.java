35a71dd302f397c3bcf282f5fb36bad5c3ecef2a
Jason Chan <jchan61@stuy.edu>
2024-09-29 23:18:45 -0400

    added test cases for both funcs

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -12,5 +12,22 @@ public class MyArrays{
   public static void main(String[]args){
     int[] test = new int[]{1};
     System.out.println(aryToString(test));
+
+    int[] old1 = new int[];
+    int[] old2 = new int[]{5,4,3,2,1};
+    int[] new1 = returnCopy(old1);
+    int[] new2 = returnCopy(old2);
+    System.out.println("first old is " + old1 + "first new is " + new1 + "same address is " old1 == new1);
+    System.out.println("second old is " + old2 + "second new is " + new2 + "same address is " old2 == new2);
+
+    int[] original = new int[]{11,22,33};
+    int[] add = new int[]{55,44,33,22,11};
+    System.out.println(concatArray(original, add));
+    original = new int[4];
+    add = new int[]{55,44,33,22,11};
+    System.out.println(concatArray(original, add));
+    original = new int[]{11,22,33};
+    add = new int[4];
+    System.out.println(concatArray(original, add));
   }
 }
