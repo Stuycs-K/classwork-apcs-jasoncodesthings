b150d5fc5de3e722eab959bef94c4d978500aaca
Jason Chan <jchan61@stuy.edu>
2024-10-01 13:28:50 -0400

    completed swapRC func and used arrToString to write test cases for it

--- 2024-09-30/ArrayMethods.java
+++ 2024-09-30/ArrayMethods.java
@@ -60,12 +60,18 @@ public class ArrayMethods {
     * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
     */
   public static int[][] swapRC(int[][]nums){
-    return new int[1][1];
+    int[][] answer = new int[nums[0].length][nums.length];
+    for (int i = 0; i < nums.length; i++){
+      for (int j = 0; j < nums[i].length; j++){
+        answer[j][i] = nums[i][j];
+      }
+    }
+    return answer;
   }
 
   public static void main (String[] args) {
 
-    //testing arrToString(int[][]ary)
+    System.out.println("Testing arrToString(int[][]ary)");
     int[][] ary1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     System.out.println("Expected: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] | Return: " + arrToString(ary1));
     int[][] ary2 = {{1, 2}, {4, 5}, {7, 8}};
@@ -77,12 +83,23 @@ public class ArrayMethods {
     int[][] ary5 = {{1, 2, 3}, {}, {7, 8, 9, 10}};
     System.out.println("Expected: [[1, 2, 3], [], [7, 8, 9, 10]] | Return: " + arrToString(ary5));
 
-    //testing arr2DSum
+    System.out.println("");
+    System.out.println("Testing arr2DSum");
     System.out.println("Expected: 45 | Return: " + arr2DSum(ary1));
     System.out.println("Expected: 27 | Return: " + arr2DSum(ary2));
     System.out.println("Expected: 0 | Return: " + arr2DSum(ary3));
     System.out.println("Expected: 66 | Return: " + arr2DSum(ary4));
     System.out.println("Expected: 40 | Return: " + arr2DSum(ary5));
+
+    System.out.println("");
+    System.out.println("Testing swapRC");
+    System.out.println("Expected: [[1, 4, 7], [2, 5, 8]] | Return: " + arrToString(swapRC(ary2)));
+    int[][] ary6 = {{20, 19, 18, 17}, {16, 15, 14, 13}, {12, 11, 10, 9}, {8, 7, 6, 5}, {4, 3, 2, 1}};
+    System.out.println("Expected: [[20, 16, 12, 8, 4], [19, 15, 11, 7, 3], [18, 14, 10, 6, 2], [17, 13, 9, 5, 1]] | Return: " + arrToString(swapRC(ary6)));
+    int[][] ary7 = {{1}, {2}, {3}, {4}};
+    System.out.println("Expected: [[1, 2, 3, 4]] | Return: " + arrToString(swapRC(ary7)));
+    int[][] ary8 = {{1, 2, 3, 4}};
+    System.out.println("Expected: [[1], [2], [3], [4]] | Return: " + arrToString(swapRC(ary8)));
   }
 
 }
