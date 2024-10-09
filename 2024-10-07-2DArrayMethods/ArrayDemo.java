06d42696c146b652ce8eda1f94bb1ce2112e935f
Jason Chan <jchan61@stuy.edu>
2024-10-09 09:06:31 -0400

    written test cases for all functions

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -1,6 +1,66 @@
 import java.util.Arrays;
 public class ArrayDemo{
   public static void main(String[]args){
+    System.out.println("Testing arrToString(int[][]ary)");
+    int[][] ary1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    System.out.println("Expected: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] | Return: " + arrToString(ary1));
+    int[][] ary2 = {{1, 2}, {4, 5}, {7, 8}};
+    System.out.println("Expected: [[1, 2], [4, 5], [7, 8]] | Return: " + arrToString(ary2));
+    int[][] ary3 = {{}, {}, {}};
+    System.out.println("Expected: [[], [], []] | Return: " + arrToString(ary3));
+    int[][] ary4 = {{1, 2, 3, 4}, {5, 6}, {7, 8, 9, 10, 11}};
+    System.out.println("Expected: [[1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]] | Return: " + arrToString(ary4));
+    int[][] ary5 = {{1, 2, 3}, {}, {7, 8, 9, 10}};
+    System.out.println("Expected: [[1, 2, 3], [], [7, 8, 9, 10]] | Return: " + arrToString(ary5));
+
+    System.out.println("");
+    System.out.println("Testing arr2DSum");
+    System.out.println("Expected: 45 | Return: " + arr2DSum(ary1));
+    System.out.println("Expected: 27 | Return: " + arr2DSum(ary2));
+    System.out.println("Expected: 0 | Return: " + arr2DSum(ary3));
+    System.out.println("Expected: 66 | Return: " + arr2DSum(ary4));
+    System.out.println("Expected: 40 | Return: " + arr2DSum(ary5));
+
+    System.out.println("");
+    System.out.println("Testing swapRC");
+    System.out.println("Expected: [[1, 4, 7], [2, 5, 8]] | Return: " + arrToString(swapRC(ary2)));
+    int[][] ary6 = {{20, 19, 18, 17}, {16, 15, 14, 13}, {12, 11, 10, 9}, {8, 7, 6, 5}, {4, 3, 2, 1}};
+    System.out.println("Expected: [[20, 16, 12, 8, 4], [19, 15, 11, 7, 3], [18, 14, 10, 6, 2], [17, 13, 9, 5, 1]] | Return: " + arrToString(swapRC(ary6)));
+    int[][] ary7 = {{1}, {2}, {3}, {4}};
+    System.out.println("Expected: [[1, 2, 3, 4]] | Return: " + arrToString(swapRC(ary7)));
+    int[][] ary8 = {{1, 2, 3, 4}};
+    System.out.println("Expected: [[1], [2], [3], [4]] | Return: " + arrToString(swapRC(ary8)));
+
+    System.out.println("");
+    System.out.println("Testing replaceNegative");
+    int[][] ary9 = {{-17, -5, 14}, {2, 67, -4}};
+    replaceNegative(ary9);
+    System.out.println("Expected: [[1, 0, 14], [2, 67, 0]] | Return: " + arrToString(ary9));
+    int[][] ary10 = {{17, 5, 14}, {2, 67, 0}, {9, 99, -999}};
+    replaceNegative(ary10);
+    System.out.println("Expected: [[17, 5, 14], [2, 67, 0], [9, 99, 1]] | Return: " + arrToString(ary10));
+    int[][] ary11 = {{-17, -5, -14}, {-2, -67, -4}};
+    replaceNegative(ary11);
+    System.out.println("Expected: [[1, 0, 0], [0, 1, 0]] | Return: " + arrToString(ary11));
+    int[][] ary12 = {{-1, 0, 1}, {0, -1, 0}, {1, 0, -1}, {-1, 1, 0, -1}};
+    replaceNegative(ary12);
+    System.out.println("Expected: [[1, 0, 1], [0, 1, 0], [1, 0, 1], [0, 1, 0, 1]] | Return: " + arrToString(ary12));
+
+    System.out.println("");
+    System.out.println("Testing copy");
+    int[][] ary13 = {{-6, 39, 99}, {100, 101, 102, 103}};
+    int[][] ary13copy = copy(ary13);
+    System.out.println("Expected: [[-6, 39, 99], [100, 101, 102, 103]] | Return: " + arrToString(ary13copy));
+    ary13copy[1][2] = 9;
+    System.out.println("Changed the copy so that arr13copy[1][2] is now 9 instead of 102.");
+    System.out.println("Original: expected [[-6, 39, 99], [100, 101, 102, 103]] | Return: " + arrToString(ary13));
+    System.out.println("Copy: expected [-6, 39, 99], [100, 101, 9, 103]] | Return: " + arrToString(ary13copy));
+
+
+
+
+    //new test cases for arrToString(int[]), countZeros2D, and htmlTable
+    System.out.println("");
     System.out.println("Testing countZeros2D");
     int[][] test1 = new int[][]{{1, 2, 0}, {3, 0}, {0}, {100, 90, 0}};
     System.out.println("Expected 4 | Return: " + countZeros2D(test1));
@@ -15,6 +75,13 @@ public class ArrayDemo{
     System.out.println("Expected <table><tr><td>5</td></tr><tr><td>30</td><td>9</td><td>2</td></tr><tr><td>6</td><td>7</td></tr></table> | Return: " + htmlTable(test4));
     int[][] test5 = new int[][]{{4, 3}, {99}};
     System.out.println("Expected <table><tr><td>4</td><td>3</td></tr><tr><td>99</td></tr></table> | Return: " + htmlTable(test5));
+
+    System.out.println("");;
+    System.out.println("Testing arrToString(int[])");
+    int[] test6 = new int[]{};
+    System.out.println("Expected [] | Return: " + arrToString(test6) + " Match: " + Arrays.toString(test6).equals(arrToString(test6)));
+    int[] test7 = new int[]{10, 5, 9, 33};
+    System.out.println("Expected [10, 5, 9, 33] | Return: " + arrToString(test7) + " Match: " + Arrays.toString(test7).equals(arrToString(test7)));
     //write your tests here!
     //You can now use Arrays.toString(yourArray) instead of writing arrayToString again.
     //Compare Arrays.toString(yourArray) to YOUR arrayToString() method to make sure yours is correct
