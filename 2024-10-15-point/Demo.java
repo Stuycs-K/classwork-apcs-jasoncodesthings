ba703c22646d824d4e15b765858f32d040371de7
Jason Chan <jchan61@stuy.edu>
2024-10-15 10:47:06 -0500

    completed distance in demo

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return 0.0;
+    return Math.sqrt(Math.pow((a.getX() - b.getX()), 2) + Math.pow((a.getX() - b.getX()), 2));
   }
 
   public static void main(String[]args){
