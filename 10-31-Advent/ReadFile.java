417c5440e6a0bb37ef7004c0b4d2a00f16579a2f
Jason Chan <jchan61@stuy.edu>
2024-11-04 10:40:44 -0600

    completed printing every other line and containting {

--- 10-31-Advent/ReadFile.java
+++ 10-31-Advent/ReadFile.java
@@ -7,6 +7,7 @@ public class ReadFile {
 
   public static void main(String[] args) {
     //2 Opening a file requires a try/catch
+    /*
     try {
       File file = new File("ReadFile.java");//1
       Scanner input = new Scanner(file);
@@ -14,12 +15,56 @@ public class ReadFile {
       while(input.hasNext()){
         System.out.println(input.nextLine());
       }
-      input.close();//releases the file from your program
+
 
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
       System.out.println("File not found");
       return; //you can return from a void function just don't put a value.
     }
+    input.close();
+
+
+    try {
+      File file = new File("ReadFile.java");//1
+      Scanner input = new Scanner(file);
+      int index = 0;
+      while(input.hasNextLine()){
+        if (index % 2 == 0){
+          System.out.println(input.nextLine());
+          index++;
+        }
+        else{
+          input.nextLine();
+          System.out.println(" ");
+          index++;
+        }
+      }
+      input.close();//releases the file from your program
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return;
   }
+*/
+
+  try {
+    File file = new File("ReadFile.java");//1
+    Scanner input = new Scanner(file);
+    String str;
+    while(input.hasNextLine()){
+      str = input.nextLine();
+      if (str.contains("{")){
+        System.out.println(str);
+      }
+    }
+    input.close();//releases the file from your program
+
+  } catch (FileNotFoundException ex) {
+    //File not found what should you do?
+    System.out.println("File not found");
+    return;
+}
+}
 }
