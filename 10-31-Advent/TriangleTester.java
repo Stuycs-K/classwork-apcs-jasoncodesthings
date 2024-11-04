76589752770e33d9012f5201a7d34af4deffdf6d
Jason Chan <jchan61@stuy.edu>
2024-11-04 13:48:30 -0500

    completed type A

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class TriangleTester{
   public static int countTrianglesA(String filename){
-    File file = new File(filename);
-    Scanner triangles = new Scanner(file);
-    String str;
-    String[] arr;
-    int answer = 0;
 
     try{
+      File file = new File(filename);
+      Scanner triangles = new Scanner(file);
+      String str;
+      String[] arr;
+      int answer = 0;
       while(triangles.hasNextLine()){
         str = triangles.nextLine();
         arr = str.split(" ");
-        if (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]) > Integer.parseInt(arr[2]) || Integer.parseInt(arr[1]) + Integer.parseInt(arr[2]) > Integer.parseInt(arr[0]) || Integer.parseInt(arr[2]) + Integer.parseInt(arr[0]) > Integer.parseInt(arr[1])){
+        if (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]) > Integer.parseInt(arr[2]) && Integer.parseInt(arr[1]) + Integer.parseInt(arr[2]) > Integer.parseInt(arr[0]) && Integer.parseInt(arr[2]) + Integer.parseInt(arr[0]) > Integer.parseInt(arr[1])){
           answer++;
         }
       }
       triangles.close();
+      return answer;
 
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return -1;
     }
-    return answer;
   }
 
   public static void main(String[]args){
