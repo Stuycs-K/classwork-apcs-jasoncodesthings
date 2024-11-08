df12efdc65ed5c194ad4bc057ec66147c861196c
Jason Chan <jchan61@stuy.edu>
2024-11-07 20:14:12 -0500

    picked off from email by partner

---
+++ 2024-11-07-Pets/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+  public static void main(String[] args) {
+    Animal Bob = new Animal("meow", 2909, "Cthulu the Destroyer");
+    Bob.speak();
+    Bird BobII = new Bird("woof", 2908, "Cthulu the Creator", 1, "green");
+    BobII.speak();
+  }
+ }
