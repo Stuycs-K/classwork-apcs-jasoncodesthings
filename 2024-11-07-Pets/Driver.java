6008941aaef1b339813ab38ed225c89339e09a8c
Jason Chan <jchan61@stuy.edu>
2024-11-07 20:21:01 -0500

    completed getName() override and commented about what happened

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,7 @@
+//When overriding the getName() method and calling it in the Bird class, it calls the getName() method from the superclass Animal and attaches the result with "The Mighty ".
+
+
+
 public class Driver {
   public static void main(String[] args) {
     Animal Bob = new Animal("meow", 2909, "Cthulu the Destroyer");
