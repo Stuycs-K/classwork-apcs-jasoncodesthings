ac59a958c85f39f9b087d753704e5529c77734d4
Jason Chan <jchan61@stuy.edu>
2024-11-07 20:39:16 -0500

    made everyone speak

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -8,12 +8,19 @@ public class Driver {
   public static void main(String[] args) {
     Animal Bob = new Animal("meow", 2909, "Cthulu the Destroyer");
     Bob.speak();
+    System.out.println("\n");
     Bird BobII = new Bird("woof", 2908, "Cthulu the Creator", 1, "green");
     BobII.speak();
+    System.out.println("\n");
 
     Animal a1 = new Animal("roar", 10, "Pete");
     Bird b1 = new Bird("squak", 99, "Thomas", 66, "blue");
     //Bird b2 = new Animal("roar", 10, "Pete");
-    Animal a2 = new Bird("squak", 99, "Thomas", 67, "red");
+    Animal a2 = new Bird("bark", 99, "Andy", 67, "red");
+    a1.speak();
+    System.out.println("\n");
+    b1.speak();
+    System.out.println("\n");
+    a2.speak();
   }
  }
