2228c3c49c59fa1270b20287c697649153c10d9c
Jason Chan <jchan61@stuy.edu>
2024-11-07 20:37:04 -0500

    tested out the four types of objects and explained why one of them doesn't work

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,12 +1,19 @@
-//When overriding the getName() method and calling it in the Bird class, it calls the getName() method from the superclass Animal and attaches the result with "The Mighty ".
-
-
-
+/*
+When overriding the getName() method and calling it in the Bird class, it calls the getName() method from the superclass Animal and attaches the result with "The Mighty ".
+*/
+/*
+Bird b2 = new Animal(...); produced an error because that line implies that Animal is the subclass, which it isn't. The methods called on this object would not work since it may not exist in the Bird class, or have different parameters.
+*/
 public class Driver {
   public static void main(String[] args) {
     Animal Bob = new Animal("meow", 2909, "Cthulu the Destroyer");
     Bob.speak();
     Bird BobII = new Bird("woof", 2908, "Cthulu the Creator", 1, "green");
     BobII.speak();
+
+    Animal a1 = new Animal("roar", 10, "Pete");
+    Bird b1 = new Bird("squak", 99, "Thomas", 66, "blue");
+    //Bird b2 = new Animal("roar", 10, "Pete");
+    Animal a2 = new Bird("squak", 99, "Thomas", 67, "red");
   }
  }
