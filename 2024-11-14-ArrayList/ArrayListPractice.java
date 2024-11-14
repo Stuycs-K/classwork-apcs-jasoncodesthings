49f6a306cabf10f53ab2921df8ea8abf1b026cb8
Jason Chan <jchan61@stuy.edu>
2024-11-14 11:39:51 -0500

    created the replaceEmpty function

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -13,4 +13,19 @@ public class ArrayListPractice{
     }
     return answer;
   }
+  public static void replaceEmpty( ArrayList<String> original){
+    for (int i = 0; i < original.size(); i++){
+      if (original.get(i).equals("")){
+        original.set(i, "Empty");
+      }
+    }
+  }
+  //public static ArrayList<String> makeReversedList( ArrayList<String> original){
+    //return a new ArrayList that is in the reversed order of the original.
+  //}
+  //public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+    //return a new ArrayList that has all values of a and b in alternating order that is:
+    //a[0], b[0], a[1], b[1]...
+    //If one list is longer than the other, just attach the remaining values to the end.
+  //}
 }
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Driver{
   public static void main(String[]args){
-    System.out.println(ArrayListPractice.createRandomArray(12));
+    ArrayList<String>test = ArrayListPractice.createRandomArray(12);
+    ArrayList<String>test1 = ArrayListPractice.createRandomArray(20);
+    ArrayList<String>test2 = ArrayListPractice.createRandomArray(100);
+    System.out.println(test);
+    ArrayListPractice.replaceEmpty(test);
+    System.out.println(test);
+    System.out.println(test1);
+    ArrayListPractice.replaceEmpty(test1);
+    System.out.println(test1);
+    System.out.println(test2);
+    ArrayListPractice.replaceEmpty(test2);
+    System.out.println(test2);
   }
 }
