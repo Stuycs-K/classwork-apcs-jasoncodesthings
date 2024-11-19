5f89f07269962b701a82fd59d17febe25f9f97ab
Jason Chan <jchan61@stuy.edu>
2024-11-19 11:19:51 -0500

    completed position changes on day two part one

--- 2024-11-18-advent2016/DayTwo.java
+++ 2024-11-18-advent2016/DayTwo.java
@@ -9,13 +9,37 @@ public class DayTwo{
       Scanner instructions = new Scanner(file);
       String line;
       String[][] keypad = new String[][]{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
-      String position = "5";
+      int x = 1;
+      int y = 1;
       String answer = "";
 
       while (instructions.hasNextLine()){
         line = instructions.nextLine();
-        System.out.println(line);
+        for (int i = 0; i < line.length(); i++){
+          if (line.charAt(i) == 'U'){
+            if (y != 2){
+              y++;
+            }
+          }
+          else if (line.charAt(i) == 'D'){
+            if (y != 0){
+              y--;
+            }
+          }
+          else if (line.charAt(i) == 'L'){
+            if (x != 0){
+              x--;
+            }
+          }
+          else if (line.charAt(i) == 'R'){
+            if (x != 2){
+              x++;
+            }
+          }
+          System.out.println(x + ", " + y);
+        }
       }
+
       return "";
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
