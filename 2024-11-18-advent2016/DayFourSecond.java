59b9e7629537d152a3d495935ef32a64cfdac937
Jason Chan <jchan61@stuy.edu>
2024-11-19 22:19:07 -0500

    completed day 4 part 2

--- 2024-11-18-advent2016/DayFourSecond.java
+++ 2024-11-18-advent2016/DayFourSecond.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class DayFourSecond{
-  public static int sums(String filename){
+  public static String sums(String filename){
     try{
       File file = new File(filename);
       Scanner data = new Scanner(file);
@@ -18,7 +18,6 @@ public class DayFourSecond{
         name = line.substring(0, line.lastIndexOf("-"));
         name = name.replace("-", " ");
         sectorID = line.substring(line.lastIndexOf("-") + 1, line.indexOf("["));
-        System.out.println(name);
 
         for (int i = 0; i < name.length(); i++){
           if (name.charAt(i) == ' '){
@@ -26,15 +25,22 @@ public class DayFourSecond{
           }
           else{
             modify += alphabet.charAt(alphabet.indexOf(name.charAt(i)) + (Integer.parseInt(sectorID) % 26));
-            System.out.println(modify);
           }
         }
+        name = modify;
+
+        if (name.contains("north")){
+          System.out.println("Original: " + line);
+          System.out.println("Decrypted name: " + name);
+        }
+
+        modify = "";
       }
 
-    return -1;
+    return "";
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
-      return -1;
+      return "";
     }
   }
 
