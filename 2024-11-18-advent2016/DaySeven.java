562df7bc5a51b8b786e992155b99c80b43113632
Jason Chan <jchan61@stuy.edu>
2024-11-21 12:51:38 -0500

    created substrings for day 7 part 1

--- 2024-11-18-advent2016/DaySeven.java
+++ 2024-11-18-advent2016/DaySeven.java
@@ -6,16 +6,29 @@ public class DaySeven{
   public static int ipCheck(String filename){
     try{
       File file = new File(filename);
-      Scanner signal = new Scanner(file);
+      Scanner addresses = new Scanner(file);
       String line;
+      String left;
+      String middle;
+      String right;
 
-      return -1;
+      while (addresses.hasNextLine()){
+        line = addresses.nextLine();
+        left = line.substring(0, line.indexOf("["));
+        middle = line.substring(line.indexOf("[") + 1, line.indexOf("]"));
+        right = line.substring(line.indexOf("]") + 1);
+        System.out.println(left);
+        System.out.println(middle);
+        System.out.println(right);
+      }
+      return 0;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
-      return "";
-    }
-
-    public static void main(String[]args){
-      System.out.println(ipCheck(inputDaySeven.txt));
+      return 0;
     }
   }
+
+  public static void main(String[]args){
+    System.out.println(ipCheck("inputDaySeven.txt"));
+  }
+}
