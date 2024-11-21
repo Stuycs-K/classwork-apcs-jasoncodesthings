e4ec8ad48518f16f6a97eadb29c0381d3b5e329a
Jason Chan <jchan61@stuy.edu>
2024-11-21 12:56:58 -0500

    created tls checker

--- 2024-11-18-advent2016/DaySeven.java
+++ 2024-11-18-advent2016/DaySeven.java
@@ -17,15 +17,25 @@ public class DaySeven{
         left = line.substring(0, line.indexOf("["));
         middle = line.substring(line.indexOf("[") + 1, line.indexOf("]"));
         right = line.substring(line.indexOf("]") + 1);
-        System.out.println(left);
-        System.out.println(middle);
-        System.out.println(right);
+        System.out.println(tlsCheck(left));
+        System.out.println(tlsCheck(middle));
+        System.out.println(tlsCheck(right));
+        System.out.println("");
       }
       return 0;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return 0;
     }
+  }
+
+  public static boolean tlsCheck(String data){
+    for (int i = 0; i < data.length() - 3; i++){
+      if (data.charAt(i) == data.charAt(i + 3) && data.charAt(i + 1) == data.charAt(i + 2) && data.charAt(i) != data.charAt(i + 1)){
+        return true;
+      }
+    }
+    return false;
   }
 
   public static void main(String[]args){
