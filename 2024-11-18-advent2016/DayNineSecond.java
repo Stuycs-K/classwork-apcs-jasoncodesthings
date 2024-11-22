8d627bf3c7c6870ce606c63aca751fc2c8542064
Jason Chan <jchan61@stuy.edu>
2024-11-22 15:41:26 -0500

    completed day 9 part 2

--- 2024-11-18-advent2016/DayNineSecond.java
+++ 2024-11-18-advent2016/DayNineSecond.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class DayNineSecond{
-  public static int decompresser(String filename){
+  public static double decompresser(String filename){
     try{
       File file = new File(filename);
       Scanner data = new Scanner(file);
@@ -11,9 +11,9 @@ public class DayNineSecond{
       int i = 0;
       int subsequent;
       int repeat;
+      String part;
       String full = "";
-      String answer = "";
-      String part = "";
+      double answer = 0;
 
       while (data.hasNextLine()){
         line = data.nextLine();
@@ -24,65 +24,44 @@ public class DayNineSecond{
         if (full.charAt(i) == '('){
           subsequent = Integer.parseInt(full.substring(i).substring(1, full.substring(i).indexOf("x")));
           repeat = Integer.parseInt(full.substring(i).substring(full.substring(i).indexOf("x") + 1, full.substring(i).indexOf(")")));
+          part = full.substring(i).substring(full.substring(i).indexOf(")") + 1, full.substring(i).indexOf(")") + 1 + subsequent);
 
-          answer += expand(full.substring(i).substring(full.substring(i).indexOf(")") + 1, full.substring(i).indexOf(")") + 1 + subsequent), repeat);
+          answer += (double)(expand(part) * repeat);
           i += full.substring(i).indexOf(")") + 1 + subsequent;
         }
         else{
-          answer += "1";
+          answer++;
           i++;
         }
       }
 
       data.close();
-      return answer.length();
+      return answer;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return -1;
     }
   }
 
-  public static String expand(String part, int repeat){
-    String str = "";
-    String add = "";
-    int addj = 0;
-    int sub;
-    int rep;
+  public static double expand(String part){
+    int j = 0, sub, rep;
+    double partAnswer = 0;
     String part2;
-    int j = 0;
+    while (j < part.length()){
+      if (part.charAt(j) == '('){
+        sub = Integer.parseInt(part.substring(j).substring(1, part.substring(j).indexOf("x")));
+        rep = Integer.parseInt(part.substring(j).substring(part.substring(j).indexOf("x") + 1, part.substring(j).indexOf(")")));
+        part2 = part.substring(j).substring(part.substring(j).indexOf(")") + 1, part.substring(j).indexOf(")") + 1 + sub);
 
-    for (int i = 0; i < repeat; i++){
-      str += part;
-    }
-
-    while(j < str.length()){
-      if (str.charAt(j) == '('){
-        sub = Integer.parseInt(str.substring(j + 1, str.indexOf("x")));
-        rep = Integer.parseInt(str.substring(str.indexOf("x") + 1, str.indexOf(")")));
-        part2 = str.substring(str.indexOf(")") + 1, str.indexOf(")") + 1 + sub);
-        for (int i = 0; i < rep; i++){
-          add += part2;
-          addj++;
-        }
-
-        if (j == 0){
-          str = add + str.substring(str.indexOf(")") + 1 + sub);
-        }
-        else{
-          str = str.substring(0, j) + add + str.substring(str.indexOf(")") + 1 + sub);
-        }
-        System.out.println(str);
-        add = "";
-        j += addj * part2.length();
-        System.out.println(j);
-        System.out.println("length: " + str.length());
+        partAnswer += (double)(expand(part2) * rep);
+        j += part.substring(j).indexOf(")") + 1 + sub;
       }
       else{
+        partAnswer++;
         j++;
       }
     }
-
-    return "";
+    return partAnswer;
   }
 
   public static void main(String[]args){
