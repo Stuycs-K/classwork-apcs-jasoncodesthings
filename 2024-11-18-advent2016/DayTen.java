9908cf4688ee666dc90d2861973d953e57531acf
Jason Chan <jchan61@stuy.edu>
2024-11-23 11:49:40 -0500

    made progress on value instructions

--- 2024-11-18-advent2016/DayTen.java
+++ 2024-11-18-advent2016/DayTen.java
@@ -10,11 +10,25 @@ public class DayTen{
       Scanner instructions = new Scanner(file);
       String line;
       ArrayList<String> listInstructions = new ArrayList<String>();
+      ArrayList<Integer> listBots = new ArrayList<Integer>();
+      ArrayList<Integer> chip1 = new ArrayList<Integer>();
+      ArrayList<Integer> chip2 = new ArrayList<Integer>();
+      int value;
+      int botID;
       while (instructions.hasNextLine()){
         line = instructions.nextLine();
         listInstructions.add(line);
       }
       System.out.println(listInstructions);
+
+      for (int i = 0; i < listInstructions.size(); i++){
+        String step = listInstructions.get(i);
+        if (step.substring(0,5).equals("value")){
+          value = Integer.parseInt(step.substring(step.indexOf(" ") + 1, step.indexOf("g") - 1));
+          botID = Integer.parseInt(step.substring(step.indexOf("bot") + 4));
+          System.out.println(value + ", " + botID);
+        }
+      }
 
       instructions.close();
       return "";
