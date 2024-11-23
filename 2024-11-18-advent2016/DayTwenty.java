1ddcdaa1f2266606545bef977b94cf42ffc45834
Jason Chan <jchan61@stuy.edu>
2024-11-22 23:17:34 -0500

    completed day 20 part 1

--- 2024-11-18-advent2016/DayTwenty.java
+++ 2024-11-18-advent2016/DayTwenty.java
@@ -4,29 +4,41 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class DayTwenty{
-  public static int smallBlacklist(String filename){
+  public static double smallBlacklist(String filename){
     try{
       File file = new File(filename);
       Scanner ips = new Scanner(file);
       String line;
       ArrayList<String> ipsformat = new ArrayList<String>();
-      double min = 0.0, max = 9.0;
+      double min = 0.0, max = 0.0;
       int ipindex = 0;
       while (ips.hasNextLine()){
         line = ips.nextLine();
         ipsformat.add(line);
       }
-      String[][] minnmax = new String[ipsformat.size()][2];
+      String[][] temp = new String[ipsformat.size()][2];
+      double[][] minnmax = new double[ipsformat.size()][2];
       for (int i = 0; i < ipsformat.size(); i++){
-        minnmax[i] = ipsformat.get(i).split("-");
-        System.out.println(Double.parseDouble(minnmax[i][0]));
-        System.out.println(Double.parseDouble(minnmax[i][1]));
+        temp[i] = ipsformat.get(i).split("-");
+        minnmax[i] = new double[]{Double.parseDouble(temp[i][0]), Double.parseDouble(temp[i][1])};
       }
 
-      
+      while (ipindex < ipsformat.size()){
+        if (min <= minnmax[ipindex][0] && minnmax[ipindex][0] <= max + 1 && max < minnmax[ipindex][1]){
+          min = minnmax[ipindex][0];
+          max = minnmax[ipindex][1];
+          ipindex = 0;
+        }
+        else{
+          ipindex++;
+        }
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println("h");
+      }
 
       ips.close();
-      return -1;
+      return max + 1.0;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return -1;
