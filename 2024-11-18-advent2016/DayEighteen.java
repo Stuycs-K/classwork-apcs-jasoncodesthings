c89ff1790a89b3e6d40540f1aff7eec92760a217
Jason Chan <jchan61@stuy.edu>
2024-11-24 12:24:54 -0500

    finished tile function

--- 2024-11-18-advent2016/DayEighteen.java
+++ 2024-11-18-advent2016/DayEighteen.java
@@ -9,12 +9,33 @@ public class DayEighteen{
       Scanner first = new Scanner(file);
       String line;
       line = first.nextLine();
-      String next = ".......";
+      StringBuilder next = new StringBuilder();
 
       line = "." + line + ".";
+      System.out.println(line);
+      for (int i = 0; i < line.length(); i++){
+        next.append(".");
+      }
 
-      first.close()
-      return answer;
+      for (int i = 0; i < 9; i++){
+        for (int j = 1; j < line.length() - 1; j++){
+          if ((line.charAt(j - 1) == '^' && line.charAt(j) == '^' && line.charAt(j + 1) == '.') ||
+          (line.charAt(j - 1) == '.' && line.charAt(j) == '^' && line.charAt(j + 1) == '^') ||
+          (line.charAt(j - 1) == '^' && line.charAt(j) == '.' && line.charAt(j + 1) == '.') ||
+          (line.charAt(j - 1) == '.' && line.charAt(j) == '.' && line.charAt(j + 1) == '^')){
+            next.setCharAt(j, '^');
+          }
+        }
+        line = next.toString();
+        next = new StringBuilder();
+        for (int k = 0; k < line.length(); k++){
+          next.append(".");
+        }
+        System.out.println(line);
+      }
+
+      first.close();
+      return -1;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return -1;
