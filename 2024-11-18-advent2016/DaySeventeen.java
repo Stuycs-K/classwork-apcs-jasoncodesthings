c9a99a6300c47286d1be4946815f1113c7bfa805
Jason Chan <jchan61@stuy.edu>
2024-11-25 22:50:29 -0500

    finished recursive part day 17 part 1

--- 2024-11-18-advent2016/DaySeventeen.java
+++ 2024-11-18-advent2016/DaySeventeen.java
@@ -10,7 +10,9 @@ public class DaySeventeen{
       File file = new File(filename);
       Scanner input = new Scanner(file);
       String line = input.nextLine();
-      String answer = findPath(line);
+      int row = 0, column = 0;
+      System.out.println(line);
+      String answer = findPath(line, row, column, "");
 
       input.close();
       return answer;
@@ -20,10 +22,33 @@ public class DaySeventeen{
     }
   }
 
-  public static String findPath(String ID){
-    String hashed = getMD5(ID).substring(0,4);
+  public static String findPath(String ID, int row, int column, String answer){
+    String hashed = getMD5(ID + answer).substring(0,4);
+    System.out.println(" ");
     System.out.println(hashed);
-    return "";
+    String direction = "UDLR";
+    if (row == 3 && column == 3){
+      System.out.println("hi");
+      return answer;
+    }
+
+    if (hashed.charAt(0) >= 98 && row != 0){
+      System.out.println("U");
+      answer = findPath(ID, row - 1, column, answer + "U");
+    }
+    if (hashed.charAt(1) >= 98 && row != 3){
+      System.out.println("D");
+      answer = findPath(ID, row + 1, column, answer + "D");
+    }
+    if (hashed.charAt(2) >= 98 && column != 0){
+      System.out.println("L");
+      answer = findPath(ID, row, column - 1, answer + "L");
+    }
+    if (hashed.charAt(3) >= 98 && column != 3){
+      System.out.println("R");
+      answer = findPath(ID, row, column + 1, answer + "R");
+    }
+    return answer;
   }
 
   public static String getMD5(String doorID){
@@ -48,6 +73,6 @@ public class DaySeventeen{
   }
 
   public static void main(String[]args){
-    System.out.println(door4x4("inputDayNineteen.txt"));
+    System.out.println(door4x4("inputDaySeventeen.txt"));
   }
 }
