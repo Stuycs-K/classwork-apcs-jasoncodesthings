52489154462e3caefac1451c9590b044c7e862bd
Jason Chan <jchan61@stuy.edu>
2024-11-28 17:02:32 -0500

    completed checkDoor function

--- 2024-11-18-advent2016/DayThirteen.java
+++ 2024-11-18-advent2016/DayThirteen.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class DayThirteen{
   public static int numSteps(String filename){
@@ -8,14 +9,39 @@ public class DayThirteen{
       File file = new File(filename);
       Scanner input = new Scanner(file);
       int favnum = Integer.parseInt(input.nextLine());
+      ArrayList<String> beenBefore = new ArrayList<String>();
+      beenBefore.add(1 + "|" + 1);
+      String answer = findPath(favnum, 1, 1, beenBefore, 0);
+      System.out.println(checkDoor(favnum,2,1));
+      System.out.println(checkDoor(favnum,2,0));
+      System.out.println(checkDoor(favnum,1,0));
 
-      input.close()
+      input.close();
       return -1;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return -1;
     }
   }
+
+  public static String findPath(int favnum, int row, int column, ArrayList<String> beenBefore, int answer){
+    String shortest = null;
+    return "";
+    //if ((row != 0) ||
+  }
+
+  public static boolean checkDoor(int input, int x, int y){
+    int equation = x * x + 3 * x + 2 * x * y + y + y * y;
+    String bin = Integer.toBinaryString(equation + input);
+    int count = 0;
+    for (int i = 0; i < bin.length(); i++){
+      if (bin.charAt(i) == '1'){
+        count++;
+      }
+    }
+    return count % 2 == 0;
+  }
+
   public static void main(String[]args){
     System.out.println(numSteps("inputDayThirteen.txt"));
   }
