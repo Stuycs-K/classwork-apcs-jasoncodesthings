9b704cefc3a1f0bb21ba78d2e17b43186526b965
Jason Chan <jchan61@stuy.edu>
2024-12-03 00:14:54 -0500

    completed isMul that checks if a string is a valid mul

--- 2024-11-18-advent2016/D3P1.java
+++ 2024-11-18-advent2016/D3P1.java
@@ -3,24 +3,36 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class D3P1{
-  public static int actualMul(String filename){
+  public static int mulCount(String filename){
     try{
       File file = new File(filename);
       Scanner data = new Scanner(file);
       String line;
 
-      
+      System.out.println(isMul("mul(4*"));
+      System.out.println(isMul("mul(6,9!"));
+      System.out.println(isMul("mul ( 2 , 4 )"));
+      System.out.println(isMul("mul(2,4)"));
+      System.out.println(isMul("mul(5,5)"));
 
       data.close();
-      return answer;
+      return -1;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return -1;
     }
   }
 
+  public static boolean isMul(String str){
+    for (int i = 3; i < str.length(); i++){
+      if (str.charAt(i) != '(' && str.charAt(i) != ')' && str.charAt(i) != ',' && (str.charAt(i) < '0' || str.charAt(i) > '9')){
+        return false;
+      }
+    }
+    return true;
+  }
 
   public static void main(String[]args){
-    System.out.println(totalDistance("inputD3.txt"));
+    System.out.println(mulCount("inputD3.txt"));
   }
 }
