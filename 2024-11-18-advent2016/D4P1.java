c032ebe17786cc21b4a979056ed298bd4d363792
Jason Chan <jchan61@stuy.edu>
2024-12-04 00:18:21 -0500

    made XMAS check function

--- 2024-11-18-advent2016/D4P1.java
+++ 2024-11-18-advent2016/D4P1.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class D4P1{
-  public static int mulCount(String filename){
+  public static int numXMAS(String filename){
     try{
       File file = new File(filename);
       Scanner data = new Scanner(file);
@@ -12,7 +12,22 @@ public class D4P1{
         line = data.nextLine();
         total += ", " + line;
       }
-      System.out.println(total);
+      String[] arr = total.split(", ");
+      int answer = 0;
+      System.out.println(isXMAS('X', 'M', 'A', 'S'));
+      System.out.println(isXMAS('X', 'S', 'A', 'S'));
+/*
+      for (int i = 0; i < arr.length; i++){
+        for (int j = 0; j < arr[i].length(); j++){
+          if (arr[i].charAt(j) == 'X'){
+            if (j <= arr[i].length() - 4){ //right side testing
+              if (i >= 3){ //diag up right
+
+              }
+            }
+          }
+        }
+      }*/
 
       data.close();
       return -1;
@@ -22,7 +37,16 @@ public class D4P1{
     }
   }
 
+  public static int isXMAS(char char1, char char2, char char3, char char4){
+    if (char1 == 'X' && char2 == 'M' && char3 == 'A' && char4 == 'S'){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  }
+
   public static void main(String[]args){
-    System.out.println(mulCount("inputD4.txt"));
+    System.out.println(numXMAS("inputD4.txt"));
   }
 }
