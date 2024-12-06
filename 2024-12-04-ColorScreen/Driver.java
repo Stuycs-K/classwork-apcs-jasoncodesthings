4bf94772971526fe724cb20356ba21ca90a1258c
Jason Chan <jchan61@stuy.edu>
2024-12-06 00:24:48 -0500

    completed classwork

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -21,7 +21,7 @@ public class Driver{
         System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
     }
     public static void color(int m1, int m2, int m3){
-        System.out.print ("\u001b["+ m1 + ";" + m2 + ";" + m3 + "m");
+        System.out.print ("\u001b["+ m1 + ";" + m2 + 10 + ";" + m3 + "m");
     }
 
     public static void go(int row,int col){
@@ -36,18 +36,45 @@ public class Driver{
         }
     }
 
+    public static int[] randomArray(int length){
+        int[] output = new int[length];
+        for (int i = 0; i < length; i++){
+            output[i] = (int) (100* Math.random());
+        }
+        return output;
+    }
+
     public static void main(String[]args){
         System.out.print(CLEAR_SCREEN);
         System.out.print(HIDE_CURSOR);
 
-        for(int i = 0; i < 80; i++){
-            for(int j = 0; j < 31; j++){
+        for(int i = 0; i < 31; i++){
+            for(int j = 0; j < 80; j++){
                 go(i+1,j+1);
-                color(BLUE, GREEN);
+                color(BLUE, BLUE);
                 System.out.print(" ");
             }
         }
 
+        int[] rando = randomArray(3);
+        for (int i = 0; i < rando.length; i++){
+            go(1, 25*i);
+            if (rando[i] < 25){
+                color(RED, BLUE, BRIGHT);
+            }
+            else if (rando[i] > 75){
+                color(GREEN, BLUE, BRIGHT);
+            }
+            else{
+                color(WHITE, BLUE);
+            }
+            System.out.print(rando[i]);
+            go(2,25*i);
+            color(WHITE,BLUE);
+            System.out.print("hellothere");
+        }
+
+        go(31,80);
         System.out.println(RESET);
     }
 }
