7f156168ee971e2bcb5bddbce300747a0c674d92
Jason Chan <jchan61@stuy.edu>
2024-12-07 20:11:52 -0500

    completed 2024 day 6 part 1

--- 2024-11-18-advent2016/D6P1.java
+++ 2024-11-18-advent2016/D6P1.java
@@ -8,22 +8,76 @@ public class D6P1{
     try{
       File file = new File(filename);
       Scanner data = new Scanner(file);
-      String line;
-      ArrayList<String> map = new ArrayList<String>();
+      String line, direction = "north";
+      ArrayList<StringBuilder> map = new ArrayList<StringBuilder>();
+      int row = 0, column = 0, answer = 0;
 
       while (data.hasNextLine()){
         line = data.nextLine();
-        map.add(line);
+        map.add(new StringBuilder(line));
       }
-      System.out.println(map);
+
+      for (int i = 0; i < map.size(); i++){
+        for (int j = 0; j < map.get(i).length(); j++){
+          if (map.get(i).charAt(j) == '^'){
+            row = i;
+            column = j;
+          }
+        }
+      }
+
+      while (0 <= row && row < map.size() && 0 <= column && column < map.get(0).length()){
+        map.get(row).setCharAt(column, 'X');
+        if (direction.equals("north")){
+          if (row == 0 || !(map.get(row - 1).charAt(column) == '#')){
+            row--;
+          }
+          else {
+            direction = "east";
+          }
+        }
+        else if (direction.equals("east")){
+          if (column == map.get(0).length() - 1 || !(map.get(row).charAt(column + 1) == '#')){
+            column++;
+          }
+          else {
+            direction = "south";
+          }
+        }
+        else if (direction.equals("south")){
+          if (row == map.size() - 1 || !(map.get(row + 1).charAt(column) == '#')){
+            row++;
+          }
+          else {
+            direction = "west";
+          }
+        }
+        else if (direction.equals("west")){
+          if (column == 0 || !(map.get(row).charAt(column - 1) == '#')){
+            column--;
+          }
+          else {
+            direction = "north";
+          }
+        }
+      }
+
+      for (int i = 0; i < map.size(); i++){
+        for (int j = 0; j < map.get(i).length(); j++){
+          if (map.get(i).charAt(j) == 'X'){
+            answer++;
+          }
+        }
+      }
 
       data.close();
-      return -1;
+      return answer;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return -1;
     }
   }
+
   public static void main(String[]args){
     System.out.println(stepTrack("inputD6.txt"));
   }
