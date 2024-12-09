aad3520eab974bca4783c87c999a3d1fc6df8826
Jason Chan <jchan61@stuy.edu>
2024-12-09 00:55:26 -0500

    set up Warrior and completed constructor

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,6 @@
 public class Driver{
   public static void main(String[]args){
-    
+    Adventurer p1 = new Warrior("Bob",99);
+    System.out.println(p1);
   }
 }
--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,3 +1,30 @@
 public class Warrior extends Adventurer{
-  
+  private int stamina;
+
+  public Warrior(String name, int hp){
+    super(name,hp);
+    this.stamina = 100;
+  }
+
+  public String getSpecialName(){return "";}
+  public int getSpecial(){return -1;}
+  public void setSpecial(int n){}
+  public int getSpecialMax(){return -1;}
+  @Override
+  public String attack(Adventurer other){
+    System.out.println("hwer");
+    return "";
+  }
+  public String support(Adventurer other){
+    System.out.println("hi");
+    return "";
+  }
+  public String support(){
+    System.out.println("i");
+    return "";
+  }
+  public String specialAttack(Adventurer other){
+    System.out.println("e");
+    return "";
+  }
 }
