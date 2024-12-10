8739a4f90895c3b8b1f149f1c28b5e723d2fc6e0
Jason Chan <jchan61@stuy.edu>
2024-12-10 00:27:34 -0500

    completed userInput for username

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 
 public class Game{
   public static void main(String[]args){
-    Adventurer player = new Warrior("Mario", 100);
-    Adventurer enemy = new CodeWarrior("Boswer", 1000);
     Scanner userInput = new Scanner(System.in);
+    // do the rest many times
+    System.out.println("Enter username");
+    //Read one line of user input
+    String userName = userInput.nextLine();
 
-    System.out.println(player.getName() + ", " + player.getHP() + "/" + player.getmaxHP() + " HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName());
-    System.out.println(enemy.getName() + ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
+    Adventurer player = new Warrior(userName, 100);
+    Adventurer enemy = new CodeWarrior("Boswer", 1000, "c++");
+
+    System.out.println(userName + " must defeat Boswer to save the Princess!");
+    while (player.getHP() > 0 && enemy.getHP() > 0){
+      System.out.println(player.getName() + ", " + player.getHP() + "/" + player.getmaxHP() + " HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName());
+      System.out.println(enemy.getName() + ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
+      player.setHP(0);
+    }
   }
 }
