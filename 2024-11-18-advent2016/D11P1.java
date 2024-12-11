f4eeec14dc9ce49c5bef6ee8358a7994eb8067f2
Jason Chan <jchan61@stuy.edu>
2024-12-11 14:00:30 -0500

    completed 2024 day 11 part 1

--- 2024-11-18-advent2016/D11P1.java
+++ 2024-11-18-advent2016/D11P1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class D11P1{
-  public static int stoneCount(String filename){
+  public static long stoneCount(String filename){
     try{
       File file = new File(filename);
       Scanner data = new Scanner(file);
@@ -16,10 +16,27 @@ public class D11P1{
         stones.add(i);
       }
 
-      System.out.println(stones);
+      for (int i = 0; i < 25; i++){
+        for (int j = 0; j < stones.size(); j++){
+          if (stones.get(j).equals("0")){
+            stones.set(j, "1");
+          }
+          else if (stones.get(j).length() % 2 == 0){
+            stones.add(j, stones.get(j).substring(0, stones.get(j).length() / 2));
+            stones.set(j, Long.toString(Long.parseLong(stones.get(j))));
+            stones.add(j + 1, stones.get(j + 1).substring(stones.get(j + 1).length() / 2));
+            stones.set(j + 1, Long.toString(Long.parseLong(stones.get(j + 1))));
+            stones.remove(j + 2);
+            j++;
+          }
+          else{
+            stones.set(j, Long.toString(Long.parseLong(stones.get(j)) * 2024));
+          }
+        }
+      }
 
       data.close();
-      return -1;
+      return stones.size();
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return -1;
